7a79bf0b1247b52b9e5ba5dabbff539377cab543
Sergey Igoshin <sergey@csb-mirena.ru>
2022-11-10 13:56:24 +0300

    lesson_3 Templates including

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 
 class TodoPageView(TemplateView):
     template_name = "todoapp/index.html"
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        return context
