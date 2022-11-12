6156a73f94b62cfbe4518a82ac84700e4e40bdf8
Sergey Igoshin <sergey@csb-mirena.ru>
2022-11-12 13:24:41 +0300

    lesson_5 New app and user model

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -8,3 +10,6 @@ urlpatterns = [
     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
     path("todoapp/", include("todoapp.urls", namespace="todoapp")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
