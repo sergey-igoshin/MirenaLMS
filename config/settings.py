d9b5521905c7213855b51e80f49ae0dfab81a73b
Sergey Igoshin <sergey@csb-mirena.ru>
2022-11-12 08:28:27 +0300

    lesson_4 Data migration

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "markdownify.apps.MarkdownifyConfig",
     "my_filters",
     "mainapp",
     "todoapp",
